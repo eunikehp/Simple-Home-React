19d4c89a2cd9d87121826ab3ab785a60841a0714
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-23 16:17:58 +0100

    adding navbar navlink on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,45 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+import { NavLink } from "react-router-dom";
 import SimpleHomeLogo from '../app/assets/img/logo.png';
+
 
 const Header = () => {
   return (
     <Navbar dark color='primary' sticky='top' expand='md'>
-      <NavbarBrand href='/'>
-          <img src={SimpleHomeLogo} alt='simplehome logo' />
+      <NavbarBrand className='ms-5' href='/' >
+          <img src={SimpleHomeLogo} alt='simplehome logo' className='float-start' />
+          <h1 className='mt-1'>Simple Home</h1>
       </NavbarBrand>
+      <Nav className='ms-auto' navbar>
+        <NavItem>
+          <NavLink className='nav-link' to='/'>
+            <i className='fa fa-home fa-lg' /> Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/about'>
+            <i className='fa fa-info fa-lg' /> About
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/catalog'>
+            <i className='fa fa-list fa-lg' /> Catalog
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink className='nav-link' to='/contact'>
+            <i className='fa fa-address-card fa-lg' /> Contact
+          </NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   )
 };
