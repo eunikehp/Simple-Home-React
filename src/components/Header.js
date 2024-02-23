311593449e466a94bbeb0c2989964cd109093053
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-23 16:30:37 +0100

    create responsive navbar and make navbar menu open

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,34 +12,39 @@ import SimpleHomeLogo from '../app/assets/img/logo.png';
 
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <Navbar dark color='primary' sticky='top' expand='md'>
       <NavbarBrand className='ms-5' href='/' >
           <img src={SimpleHomeLogo} alt='simplehome logo' className='float-start' />
           <h1 className='mt-1'>Simple Home</h1>
       </NavbarBrand>
-      <Nav className='ms-auto' navbar>
-        <NavItem>
-          <NavLink className='nav-link' to='/'>
-            <i className='fa fa-home fa-lg' /> Home
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className='nav-link' to='/about'>
-            <i className='fa fa-info fa-lg' /> About
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className='nav-link' to='/catalog'>
-            <i className='fa fa-list fa-lg' /> Catalog
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className='nav-link' to='/contact'>
-            <i className='fa fa-address-card fa-lg' /> Contact
-          </NavLink>
-        </NavItem>
-      </Nav>
+      <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+      <Collapse navbar isOpen={menuOpen}>
+        <Nav className='ms-auto' navbar>
+          <NavItem>
+            <NavLink className='nav-link' to='/'>
+              <i className='fa fa-home fa-lg' /> Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/about'>
+              <i className='fa fa-info fa-lg' /> About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/catalog'>
+              <i className='fa fa-list fa-lg' /> Catalog
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className='nav-link' to='/contact'>
+              <i className='fa fa-address-card fa-lg' /> Contact
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   )
 };
