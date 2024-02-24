d16b929ce811959d8af48612eb8732f5b3876784
Eunike Pardede <eunikehedriani@gmail.com>
2024-02-24 09:01:41 +0100

    Header and Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import { Container, Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
 const Footer = () => {
@@ -6,8 +8,53 @@ const Footer = () => {
     <footer className= 'site-footer'>
       <Container>
         <Row>
+          <Col>
+          <h5>Location</h5>
+            <p>
+              Bronkhorststraat 30
+              <br />
+              2718 AT Den Haag, The Netherlands
+            </p>
+          <h5>Opening Hours</h5>
+            <p>
+              Monday - Saturday 10.00-18.00
+              <br />
+              Sunday 12.00-18.00
+            </p>
+          </Col>
+          <Col sm='3' className='text-center'>
+            <h5>Contact us:</h5>
+            <a
+                role='button'
+                className='btn btn-link'
+                href='tel:+31120334445'
+            >
+                <i className='fa fa-phone' /> (+31) 120 334 445
+            </a>
+            <br />
+            <a
+                role='button'
+                className='btn btn-link'
+                href='mailto:hello@simplehome.nl'
+            >
+                <i className='fa fa-envelope-o' /> hello@simplehome.nl
+            </a>
+          </Col>
           <Col xs={{ size: 4, offset: 1 }} sm='2'>
-            <h5>this will be site navigation!</h5>
+              <ul className='list-unstyled text-center'>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+                <li>
+                  <Link to='/catalog'>Catalog</Link>
+                </li>
+                <li>
+                  <Link to='/contact'>Contact</Link>
+                </li>
+              </ul>
           </Col>
           <Col xs='6' sm='3' className='text-center'>
             <h5>Follow us:</h5>
@@ -36,23 +83,7 @@ const Footer = () => {
                 <i className='fa fa-youtube' />
             </a>
           </Col>
-          <Col sm='4' className='text-center'>
-            <a
-                role='button'
-                className='btn btn-link'
-                href='tel:+31120334445'
-            >
-                <i className='fa fa-phone' /> (+31) 120 334 445
-            </a>
-            <br />
-            <a
-                role='button'
-                className='btn btn-link'
-                href='mailto:hello@simplehome.nl'
-            >
-                <i className='fa fa-envelope-o' /> hello@simplehome.nl
-            </a>
-          </Col>
+          <p className='text-center'>2024 Simple Home. Site by Eunike Hedriani Pardede</p>
         </Row>
       </Container>
     </footer>
